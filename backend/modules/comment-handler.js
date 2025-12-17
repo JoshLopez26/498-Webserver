c69a6e8686d8f933eee14124d0c0ca26db4a48ec
Joshua Lopez <burningrubber46@gmail.com>
2025-12-17 18:13:12 -0500

    fixed naming issue

--- backend/modules/comment-handler.js
+++ backend/modules/comment-handler.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('../db');
+const db = require('../database');
 const { requireAuth } = require('./auth-middleware');
 
 function loadComments(userId, currentPage, PAGE_SIZE) {
